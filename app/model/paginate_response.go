abd0124714795866b2a2de4226b8e5bdfed07b24
Saiful I. Wicaksana <icaksama@gmail.com>
2023-01-12 18:50:37 +0700

    add paginate and token response models

--- app/model/paginate_response.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type PaginateResponse struct {
-	Code    int         `json:"code"`
-	Message string      `json:"message"`
-	Next    string      `json:"next"`
-	Data    interface{} `json:"data"`
-}
--- app/model/web_response.go
+++ app/model/web_response.go
@@ -5,3 +5,17 @@ type WebResponse struct {
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 }
+
+type PaginateResponse struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Next    string      `json:"next"`
+	Data    interface{} `json:"data"`
+}
+
+type TokenResponse struct {
+	Type   string      `json:"type"`
+	Alg    string      `json:"alg"`
+	Token  string      `json:"token"`
+	Claims interface{} `json:"claims"`
+}
