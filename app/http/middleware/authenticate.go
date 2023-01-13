72aebcccfa7d8a4956c0072b896b49a1912da1d3
Saiful I. Wicaksana <icaksama@gmail.com>
2023-01-13 12:50:37 +0700

    update authenticate check jwt is valid

--- app/http/middleware/authenticate.go
+++ app/http/middleware/authenticate.go
@@ -1,10 +1,17 @@
 package middleware
 
 import (
+	"govel/app/exception"
+	"govel/app/helper"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func Authenticate(c *fiber.Ctx) error {
-	//
+	token := helper.ParseECDSAToken(c.FormValue("token", ""), jwt.SigningMethodES256)
+	if !token.Valid {
+		exception.PanicResponse("Token invalid.")
+	}
 	return c.Next()
 }
