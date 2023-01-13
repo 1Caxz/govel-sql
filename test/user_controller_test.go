c6f56f8a726b8af4c6937e6ccec679381f7deced
Saiful I. Wicaksana <icaksama@gmail.com>
2023-01-13 12:55:23 +0700

    add refresh token test

--- test/user_controller_test.go
+++ test/user_controller_test.go
@@ -88,7 +88,7 @@ func TestUserController_Register(t *testing.T) {
 
 func TestUserController_Login(t *testing.T) {
 	// Setup form data
-	data := strings.NewReader("email=govel63bf26a363c2a@gmail.com&password=rahasia")
+	data := strings.NewReader("email=consequatur@gmail.com&password=rahasia")
 
 	// Setup request
 	request := httptest.NewRequest("POST", "/api/v1/users/login", data)
@@ -124,6 +124,48 @@ func TestUserController_Login(t *testing.T) {
 	json.Unmarshal(jsonClaims, &loginUserResponse)
 	assert.NotEmpty(t, loginUserResponse.Name)
 	assert.Equal(t, request.FormValue("email"), loginUserResponse.Email)
+	assert.Equal(t, 1, loginUserResponse.Role)
+}
+
+func TestUserController_RefreshToken(t *testing.T) {
+	// Setup form data
+	data := strings.NewReader("token=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsInNvY2lhbF9pZCI6IiIsImVtYWlsIjoiY29uc2VxdWF0dXJAZ21haWwuY29tIiwibmljayI6Im9jY2FlY2F0aSIsIm5hbWUiOiJlaXVzIiwicGljIjoiL2Fzc2V0cy9zdGF0aWMvdXNlci5wbmciLCJsb2NhdGlvbiI6IkluZG9uZXNpYSIsImRlc2MiOiIiLCJyb2xlIjoxfQ.8SoZMH7f_iReJ-YQmks92QAXo_TjRzrO3xtsdnD_6bvFcJiwXiBSmr1D_7oHEtjdkHNJpeWGCy_yhDiTlzZSXQ")
+
+	// Setup request
+	request := httptest.NewRequest("POST", "/api/v1/users/refresh-token", data)
+
+	// Setup header
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Set("Accept", "application/x-www-form-urlencoded")
+
+	// Test the request
+	response, _ := app.Test(request)
+	// assert.Equal(t, 200, response.StatusCode)
+
+	// Test default json result
+	responseBody, _ := io.ReadAll(response.Body)
+	webResponse := model.WebResponse{}
+	json.Unmarshal(responseBody, &webResponse)
+	assert.Equal(t, 200, webResponse.Code)
+	assert.Equal(t, "OK", webResponse.Message)
+
+	// Test response data
+	jsonData, _ := json.Marshal(webResponse.Data)
+	tokenResponse := model.TokenResponse{}
+	json.Unmarshal(jsonData, &tokenResponse)
+	assert.Equal(t, "bearer", tokenResponse.Type)
+	assert.Equal(t, "es256", tokenResponse.Alg)
+
+	// Check token is valid
+	token := helper.ParseECDSAToken(tokenResponse.Token, jwt.SigningMethodES256)
+	assert.True(t, token.Valid)
+
+	jsonClaims, _ := json.Marshal(tokenResponse.Claims)
+	refreshTokenUserResponse := model.RefreshTokenUserResponse{}
+	json.Unmarshal(jsonClaims, &refreshTokenUserResponse)
+	assert.NotEmpty(t, refreshTokenUserResponse.Name)
+	assert.NotEmpty(t, refreshTokenUserResponse.Email)
+	assert.Equal(t, 1, refreshTokenUserResponse.Role)
 }
 
 func TestUserController_Show(t *testing.T) {
@@ -152,10 +194,11 @@ func TestUserController_Show(t *testing.T) {
 
 func TestUserController_Update(t *testing.T) {
 	// Setup form data
-	data := strings.NewReader("name=Saiful Wicaksana&location=Jakarta&desc=Engineer")
+	token := "token=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsInNvY2lhbF9pZCI6IiIsImVtYWlsIjoiY29uc2VxdWF0dXJAZ21haWwuY29tIiwibmljayI6Im9jY2FlY2F0aSIsIm5hbWUiOiJlaXVzIiwicGljIjoiL2Fzc2V0cy9zdGF0aWMvdXNlci5wbmciLCJsb2NhdGlvbiI6IkluZG9uZXNpYSIsImRlc2MiOiIiLCJyb2xlIjoxfQ.8SoZMH7f_iReJ-YQmks92QAXo_TjRzrO3xtsdnD_6bvFcJiwXiBSmr1D_7oHEtjdkHNJpeWGCy_yhDiTlzZSXQ"
+	data := strings.NewReader(token + "&name=Saiful Wicaksana&location=Jakarta&desc=Engineer")
 
 	// Setup request
-	request := httptest.NewRequest("POST", "/api/v1/users/update/3", data)
+	request := httptest.NewRequest("POST", "/api/v1/users/update/11", data)
 
 	// Setup header
 	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
@@ -183,8 +226,11 @@ func TestUserController_Update(t *testing.T) {
 }
 
 func TestUserController_Delete(t *testing.T) {
+	// Setup form data
+	data := strings.NewReader("token=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic29jaWFsX2lkIjoiIiwiZW1haWwiOiJxdWlAZ21haWwuY29tIiwibmljayI6InNpbnQiLCJuYW1lIjoibWluaW1hIiwicGljIjoiL2Fzc2V0cy9zdGF0aWMvdXNlci5wbmciLCJsb2NhdGlvbiI6IkluZG9uZXNpYSIsImRlc2MiOiIiLCJyb2xlIjoxfQ.fAT_hDCcUq7eL7CfI-Z6UIOhxTChJeeScR9-BcaoA86nxw_7z5RuSfZa59DFRJApabm-s1TeFSE92aw4w2pwng")
+
 	// Setup request
-	request := httptest.NewRequest("POST", "/api/v1/users/delete/1", nil)
+	request := httptest.NewRequest("POST", "/api/v1/users/delete/1", data)
 
 	// Setup header
 	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
