4eccc73ad00e5e31418f9dd1dda794514753067e
Saiful I. Wicaksana <icaksama@gmail.com>
2023-01-13 12:54:28 +0700

    add refresh token route and implement authenticate middleware

--- app/http/controller/user_controller.go
+++ app/http/controller/user_controller.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"govel/app/exception"
 	"govel/app/helper"
+	"govel/app/http/middleware"
 	"govel/app/model"
 	"govel/app/service"
 	"strconv"
@@ -26,9 +27,10 @@ func (controller *UserController) Route(route fiber.Router) {
 	group.Get("/search/:query", controller.Search)
 
 	group.Post("/login", controller.Login)
+	group.Post("/refresh-token", controller.RefreshToken).Name("refresh-token")
 	group.Post("/register", controller.Register)
-	group.Post("/update/:id", controller.Update)
-	group.Post("/delete/:id", controller.Delete)
+	group.Post("/update/:id", middleware.Authenticate, controller.Update)
+	group.Post("/delete/:id", middleware.Authenticate, controller.Delete)
 
 	// Add this endpoint at the bottom to avoid the path conflict
 	group.Get("/:id", controller.Show)
@@ -97,14 +99,41 @@ func (ctx *UserController) Login(c *fiber.Ctx) error {
 
 	token := helper.MakeECDSAToken(&data, jwt.SigningMethodES256)
 
+	refreshTokenURL, err := c.GetRouteURL("refresh-token", nil)
+	exception.PanicIfNeeded(err)
+
 	return c.Status(200).JSON(model.WebResponse{
 		Code:    200,
 		Message: "OK",
 		Data: model.TokenResponse{
-			Type:   "bearer",
-			Alg:    "es256",
-			Token:  token,
-			Claims: data,
+			Type:            "bearer",
+			Alg:             "es256",
+			RefreshTokenURL: c.BaseURL() + refreshTokenURL,
+			Token:           token,
+			Claims:          data,
+		},
+	})
+}
+
+func (ctx *UserController) RefreshToken(c *fiber.Ctx) error {
+	data := ctx.service.RefreshToken(model.RefreshTokenUserRequest{
+		Token: c.FormValue("token"),
+	})
+
+	token := helper.MakeECDSAToken(&data, jwt.SigningMethodES256)
+
+	refreshTokenURL, err := c.GetRouteURL("refresh-token", nil)
+	exception.PanicIfNeeded(err)
+
+	return c.Status(200).JSON(model.WebResponse{
+		Code:    200,
+		Message: "OK",
+		Data: model.TokenResponse{
+			Type:            "bearer",
+			Alg:             "es256",
+			RefreshTokenURL: c.BaseURL() + refreshTokenURL,
+			Token:           token,
+			Claims:          data,
 		},
 	})
 }
@@ -144,9 +173,8 @@ func (ctx *UserController) Update(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	exception.PanicIfNeeded(err)
 
-	fmt.Printf("Value name: %s", c.Params("name"))
-
 	data := ctx.service.Update(model.UpdateUserRequest{
+		Token:    c.FormValue("token"),
 		Id:       id,
 		Name:     c.FormValue("name"),
 		Location: c.FormValue("location"),
@@ -166,7 +194,8 @@ func (ctx *UserController) Delete(c *fiber.Ctx) error {
 	exception.PanicIfNeeded(err)
 
 	data := ctx.service.Delete(model.DeleteUserRequest{
-		Id: id,
+		Token: c.FormValue("token"),
+		Id:    id,
 	})
 
 	// Return non pagination response
